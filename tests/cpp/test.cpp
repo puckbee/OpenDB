3e7074dcf44d79d8e2ab41aad309216ba0f4cdcc
puckbee@gmail.com <puckbee@gmail.com>
2020-09-12 21:51:17 +0800

    cpp test on how to use openDB

--- tests/cpp/test.cpp
+++ tests/cpp/test.cpp
@@ -1,9 +1,18 @@
 #include "db.h"
+#include "lefin.h"
+
 #include <iostream>
 using namespace odb;
+using namespace std;
 int main(int argc, char *argv[])
 {
-    dbDatabase* db = dbDatabase::create();
-    std::cout<<"2"<<std::endl;
-    return 0;
+  dbDatabase* db = dbDatabase::create();
+  lefin lefReader(db, false);
+  dbLib* lib = lefReader.createTechAndLib("gscl45nm", "/home/xiebiwei/Projects/OpenDB/tests/data/gscl45nm.lef");
+  cout<<lib->getName()<<"; "<< (lib->getSites().begin())->getHeight()<<"; "<<endl;
+  cout<<endl;
+
+
+//    std::cout<<"2"<<std::endl;
+  return 0;
 }
